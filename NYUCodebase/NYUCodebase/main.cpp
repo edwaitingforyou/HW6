5fc5a431da172be975e31dcc0f8b97dc036344d2
BH Chen <edwardcbh1993@gmail.com>
2015-04-18 15:41:08 -0400

    hw6

--- NYUCodebase/NYUCodebase/main.cpp
+++ NYUCodebase/NYUCodebase/main.cpp
@@ -12,70 +12,9 @@
 #include <random>
 using namespace std;
 
-typedef struct {
-	float x;
-	float y;
-
-	float r;
-	float g;
-	float b;
-} Vertex2D;
-
-GLuint LoadTexture(const char *image_path)
-{
-	SDL_Surface *surface = IMG_Load(image_path);
-
-	GLuint textureID;
-	glGenTextures(1, &textureID);
-	glBindTexture(GL_TEXTURE_2D, textureID);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA,
-		GL_UNSIGNED_BYTE, surface->pixels);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	SDL_FreeSurface(surface);
-	return textureID;
-}
-
-class SheetSprite
-{
-public:
-	SheetSprite() :
-		textureID(0), u(0), v(v), width(0), height(0){}
-	SheetSprite(unsigned int textureID, float u, float v, float width, float height) :
-		textureID(textureID), u(u), v(v), width(width), height(height){}
-	float scale;
-	unsigned int textureID;
-	float x;
-	float y;
-	float u;
-	float v;
-	float width;
-	float height;
-
-	void Draw(float scale = 1.0)
-	{
-		glEnable(GL_TEXTURE_2D);
-		glBindTexture(GL_TEXTURE_2D, textureID);
-
-		glMatrixMode(GL_MODELVIEW);
-
-		GLfloat quad[] = { -width * scale, height * scale, -width * scale, -height * scale,
-			width * scale, -height * scale, width * scale, height * scale };
-		glVertexPointer(2, GL_FLOAT, 0, quad);
-		glEnableClientState(GL_VERTEX_ARRAY);
-		GLfloat quadUVs[] = { u, v, u, v + height, u + width, v + height, u + width, v };
-		glTexCoordPointer(2, GL_FLOAT, 0, quadUVs);
-		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-		glDrawArrays(GL_QUADS, 0, 4);
-		glDisable(GL_TEXTURE_2D);
-	}
-};
+#define FIXED_TIMESTEP 0.0166666f
+#define MAX_TIMESTEPS 6
+float timeLeftOver = 0.0f;
 
 class Vector 
 {
@@ -95,13 +34,85 @@ public:
 	union
 	{ 
 		float m[4][4];
-		float ml[16];
+		float ml[16]; 
 	};  
-	void identity();
+	Matrix()
+	{
+		m[0][0] = 0.0f;
+		m[0][1] = 0.0f;
+		m[0][2] = 0.0f;
+		m[0][3] = 0.0f;
+		m[1][0] = 0.0f;
+		m[1][1] = 0.0f;
+		m[1][2] = 0.0f;
+		m[1][3] = 0.0f;
+		m[2][0] = 0.0f;
+		m[2][1] = 0.0f;
+		m[2][2] = 0.0f;
+		m[2][3] = 0.0f;
+		m[3][0] = 0.0f;
+		m[3][1] = 0.0f;
+		m[3][2] = 0.0f;
+		m[3][3] = 0.0f;
+	}
+	Matrix(const Matrix& m2)
+	{
+		for (int i = 0; i <= 3; i++)
+		{
+			for (int p = 0; p <= 3; p++)
+			{
+				m[i][p] = m2.m[i][p];
+			}
+		}
+	}
+	void identity()
+	{
+		Matrix tmp;
+		tmp.m[0][0] = 1.0f;
+		tmp.m[1][1] = 1.0f;
+		tmp.m[2][2] = 1.0f;
+		tmp.m[3][3] = 1.0f;
+		*this = *this*tmp;
+	}
 	Matrix inverse();
 	Matrix operator * (const Matrix& m2);
 	Vector operator * (const Vector& v2);
 };
+
+Matrix translate(float x, float y)
+{
+	Matrix tmp;
+	tmp.m[0][0] = 1.0f;
+	tmp.m[1][1] = 1.0f;
+	tmp.m[2][2] = 1.0f;
+	tmp.m[3][3] = 1.0f;
+	tmp.m[3][0] = x;
+	tmp.m[3][1] = y;
+	tmp.m[3][2] = 1.0f;
+	return tmp;
+}
+
+Matrix scale(float x, float y)
+{
+	Matrix tmp;
+	tmp.m[0][0] = x;
+	tmp.m[1][1] = y;
+	tmp.m[2][2] = 1.0f;
+	tmp.m[3][3] = 1.0f;
+	return tmp;
+}
+
+Matrix rotate(float a)
+{
+	Matrix tmp;
+	tmp.m[0][0] = cos(a);
+	tmp.m[1][0] = -sin(a);
+	tmp.m[0][1] = sin(a);
+	tmp.m[1][1] = cos(a);
+	tmp.m[2][2] = 1.0f;
+	tmp.m[3][3] = 1.0f;
+	return tmp;
+}
 
 Vector Matrix::operator *(const Vector& v2)
 {
@@ -110,22 +121,22 @@ Vector Matrix::operator *(const Vector& v2)
 
 Matrix Matrix::operator * (const Matrix& m2)
 {
-	float m00 = m[0][0] * m2.m[0][0] + m[0][1] * m2.m[1][0] + m[0][2] * m2.m[2][0] + m[0][3] * m2.m[3][0];
-	float m01 = m[0][0] * m2.m[0][1] + m[0][1] * m2.m[1][1] + m[0][2] * m2.m[2][1] + m[0][3] * m2.m[3][1];
-	float m02 = m[0][0] * m2.m[0][2] + m[0][1] * m2.m[1][2] + m[0][2] * m2.m[2][2] + m[0][3] * m2.m[3][2];
-	float m03 = m[0][0] * m2.m[0][3] + m[0][1] * m2.m[1][3] + m[0][2] * m2.m[2][3] + m[0][3] * m2.m[3][3];
-	float m10 = m[1][0] * m2.m[0][0] + m[1][1] * m2.m[1][0] + m[1][2] * m2.m[2][0] + m[1][3] * m2.m[3][0];
-	float m11 = m[1][0] * m2.m[0][1] + m[1][1] * m2.m[1][1] + m[1][2] * m2.m[2][1] + m[1][3] * m2.m[3][1];
-	float m12 = m[1][0] * m2.m[0][2] + m[1][1] * m2.m[1][2] + m[1][2] * m2.m[2][2] + m[1][3] * m2.m[3][2];
-	float m13 = m[1][0] * m2.m[0][3] + m[1][1] * m2.m[1][3] + m[1][2] * m2.m[2][3] + m[1][3] * m2.m[3][3];
-	float m20 = m[2][0] * m2.m[0][0] + m[2][1] * m2.m[1][0] + m[2][2] * m2.m[2][0] + m[2][3] * m2.m[3][0];
-	float m21 = m[2][0] * m2.m[0][1] + m[2][1] * m2.m[1][1] + m[2][2] * m2.m[2][1] + m[2][3] * m2.m[3][1];
-	float m22 = m[2][0] * m2.m[0][2] + m[2][1] * m2.m[1][2] + m[2][2] * m2.m[2][2] + m[2][3] * m2.m[3][2];
-	float m23 = m[2][0] * m2.m[0][3] + m[2][1] * m2.m[1][3] + m[2][2] * m2.m[2][3] + m[2][3] * m2.m[3][3];
-	float m30 = m[3][0] * m2.m[0][0] + m[3][1] * m2.m[1][0] + m[3][2] * m2.m[3][0] + m[3][3] * m2.m[3][0];
-	float m31 = m[3][0] * m2.m[0][1] + m[3][1] * m2.m[1][1] + m[3][2] * m2.m[3][1] + m[3][3] * m2.m[3][1];
-	float m32 = m[3][0] * m2.m[0][2] + m[3][1] * m2.m[1][2] + m[3][2] * m2.m[3][2] + m[3][3] * m2.m[3][2];
-	float m33 = m[3][0] * m2.m[0][3] + m[3][1] * m2.m[1][3] + m[3][2] * m2.m[3][3] + m[3][3] * m2.m[3][3];
+	float m00 = m2.m[0][0] * m[0][0] + m2.m[0][1] * m[1][0] + m2.m[0][2] * m[2][0] + m2.m[0][3] * m[3][0];
+	float m01 = m2.m[0][0] * m[0][1] + m2.m[0][1] * m[1][1] + m2.m[0][2] * m[2][1] + m2.m[0][3] * m[3][1];
+	float m02 = m2.m[0][0] * m[0][2] + m2.m[0][1] * m[1][2] + m2.m[0][2] * m[2][2] + m2.m[0][3] * m[3][2];
+	float m03 = m2.m[0][0] * m[0][3] + m2.m[0][1] * m[1][3] + m2.m[0][2] * m[2][3] + m2.m[0][3] * m[3][3];
+	float m10 = m2.m[1][0] * m[0][0] + m2.m[1][1] * m[1][0] + m2.m[1][2] * m[2][0] + m2.m[1][3] * m[3][0];
+	float m11 = m2.m[1][0] * m[0][1] + m2.m[1][1] * m[1][1] + m2.m[1][2] * m[2][1] + m2.m[1][3] * m[3][1];
+	float m12 = m2.m[1][0] * m[0][2] + m2.m[1][1] * m[1][2] + m2.m[1][2] * m[2][2] + m2.m[1][3] * m[3][2];
+	float m13 = m2.m[1][0] * m[0][3] + m2.m[1][1] * m[1][3] + m2.m[1][2] * m[2][3] + m2.m[1][3] * m[3][3];
+	float m20 = m2.m[2][0] * m[0][0] + m2.m[2][1] * m[1][0] + m2.m[2][2] * m[2][0] + m2.m[2][3] * m[3][0];
+	float m21 = m2.m[2][0] * m[0][1] + m2.m[2][1] * m[1][1] + m2.m[2][2] * m[2][1] + m2.m[2][3] * m[3][1];
+	float m22 = m2.m[2][0] * m[0][2] + m2.m[2][1] * m[1][2] + m2.m[2][2] * m[2][2] + m2.m[2][3] * m[3][2];
+	float m23 = m2.m[2][0] * m[0][3] + m2.m[2][1] * m[1][3] + m2.m[2][2] * m[2][3] + m2.m[2][3] * m[3][3];
+	float m30 = m2.m[3][0] * m[0][0] + m2.m[3][1] * m[1][0] + m2.m[3][2] * m[3][0] + m2.m[3][3] * m[3][0];
+	float m31 = m2.m[3][0] * m[0][1] + m2.m[3][1] * m[1][1] + m2.m[3][2] * m[3][1] + m2.m[3][3] * m[3][1];
+	float m32 = m2.m[3][0] * m[0][2] + m2.m[3][1] * m[1][2] + m2.m[3][2] * m[3][2] + m2.m[3][3] * m[3][2];
+	float m33 = m2.m[3][0] * m[0][3] + m2.m[3][1] * m[1][3] + m2.m[3][2] * m[3][3] + m2.m[3][3] * m[3][3];
 	Matrix result;
 	result.m[0][0] = m00;
 	result.m[0][1] = m01;
@@ -240,28 +251,18 @@ public:
 		this->y = y;
 		this->x_velocity = vx;
 		this->y_velocity = vy;
-		matrix.m[0][0] = x;
-		matrix.m[0][1] = 0;
-		matrix.m[0][2] = 0;
-		matrix.m[0][3] = 0;
-		matrix.m[1][0] = y;
-		matrix.m[1][1] = 0;
-		matrix.m[1][2] = 0;
-		matrix.m[1][3] = 0;
-		matrix.m[2][0] = 0;
-		matrix.m[2][1] = 0;
-		matrix.m[2][2] = 0;
-		matrix.m[2][3] = 0;
-		matrix.m[3][0] = 0;
-		matrix.m[3][1] = 0;
-		matrix.m[3][2] = 0;
-		matrix.m[3][3] = 0;
+		angle = 0.0f;
+		scalex = 1.0f;
+		scaley = 1.0f;
 	}
 	Entity() :x(0.0), y(0.0), y_velocity(0.0), x_velocity(0.0), top(false), bottom(false), right(false), left(false){}
 	Matrix matrix;
 	Vector vector;
 	float x;
 	float y;
+	float scalex;
+	float scaley;
+	float angle;
 	float width;
 	float height;
 	float y_velocity;
@@ -270,14 +271,35 @@ public:
 	bool bottom;
 	bool right;
 	bool left;
+	void build();
+	void draw();
 };
+
+void Entity::build()
+{
+	matrix = scale(scalex,scaley)*rotate(angle)*translate(x, y);
+}
+
+void Entity::draw()
+{
+	build();
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glMultMatrixf(matrix.ml);
+	GLfloat quad[] = { -0.10f, 0.10f, -0.10f, -0.10f, 0.10f, -0.10f, 0.10f, 0.10f };
+	glVertexPointer(2, GL_FLOAT, 0, quad);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glDrawArrays(GL_QUADS, 0, 4);
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glPopMatrix();
+}
 
 class Game
 {
 private:
 	bool done;
 	SDL_Window* displayWindow;
-	vector<SheetSprite> Sprite;
+	//vector<SheetSprite> Sprite;
 	int vertexnumber;
 	vector<Entity> entities;
 	vector<unsigned int> font;
@@ -287,7 +309,6 @@ private:
 	float gravity;
 	float x_friction;
 	float y_friction;
-	Matrix matrix;
 public:
 	void clearup();
 	void initialize();
@@ -320,10 +341,6 @@ void Game::initialize()
 	glOrtho(-1.33, 1.33, -1.0, 1.0, -1.0, 1.0);
 	glMatrixMode(GL_MODELVIEW);
 	glViewport(0, 0, 800, 600);
-	GLuint sheet = LoadTexture("sheet.png");
-	unsigned int testSheet = LoadTexture("sheet.png");
-	SheetSprite block_1(testSheet, 224.0 / 1024.0, 748.0 / 1024.0, 101.0 / 1024.0, 84.0 / 1024.0);
-	Sprite.push_back(block_1);
 	framesPerSecond = 30.0f;
 	elapsed = 0.0f;
 	lastFrameTicks = 0.0f;
@@ -333,7 +350,7 @@ void Game::initialize()
 		float y = float((rand() % 100 - 50) / 50.0);
 		float xspeed = float((rand() % 100 - 50) / 50.0);
 		float yspeed = float((rand() % 100 - 50) / 50.0);
-		entities.push_back(Entity(x, y, xspeed, yspeed));
+		entities.push_back(Entity(x,y,xspeed,yspeed));
 	}
 }
 
@@ -347,35 +364,67 @@ void Game::update()
 			done = true;
 		}
 	}
+	float ticks = (float)SDL_GetTicks() / 1000.0f;
+	elapsed = ticks - lastFrameTicks;
+	lastFrameTicks = ticks;
+	float fixedElapsed = elapsed + timeLeftOver;
+	if (fixedElapsed > FIXED_TIMESTEP * MAX_TIMESTEPS)
+	{
+		fixedElapsed = FIXED_TIMESTEP * MAX_TIMESTEPS;
+	}
+	while (fixedElapsed >= FIXED_TIMESTEP)
+	{
+		fixedElapsed -= FIXED_TIMESTEP;
+		collision();
+	}
+	timeLeftOver = fixedElapsed;
+}
+
+bool Game::entity_collide(const Entity &entity1, const Entity &entity2)
+{
+	return (abs(float(entity1.x - entity2.x))*abs(float(entity1.x - entity2.x)) + abs(float(entity1.y - entity2.y))*abs(float(entity1.y - entity2.y))) < 0.2*0.2;
+}
+
+void Game::collision()
+{
+	for (int i = 0; i < entities.size(); i++)
+	{
+		entities[i].x += entities[i].x_velocity * FIXED_TIMESTEP;
+		for (int p = 0; p < entities.size(); p++)
+		{
+			if (entity_collide(entities[i], entities[p]))
+			{
+				entities[i].x += 0.0001*entities[i].x_velocity;
+				entities[i].x_velocity = -entities[i].x_velocity;
+				entities[p].x += 0.0001*entities[p].x_velocity;
+				entities[p].x_velocity = -entities[p].x_velocity;
+			}
+			if (entities[i].x>1.33 || entities[i].x < -1.33)
+				entities[i].x_velocity = -entities[i].x_velocity;
+		}
+		entities[i].y += entities[i].y_velocity * FIXED_TIMESTEP;
+		for (int p = 0; p < entities.size(); p++)
+		{
+			if (entity_collide(entities[i], entities[p]))
+			{
+				entities[i].y += 0.0001*entities[i].y_velocity;
+				entities[i].y_velocity = -entities[i].y_velocity;
+				entities[p].y += 0.0001*entities[p].y_velocity;
+				entities[p].y_velocity = -entities[p].y_velocity;
+			}
+			if (entities[i].y>1.00 || entities[i].y < -1.00)
+				entities[i].y_velocity = -entities[i].y_velocity;
+		}
+	}
 }
 
 void Game::render()
 {	
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
+	glClear(GL_COLOR_BUFFER_BIT);
 	for (int i = 0; i < entities.size(); i++)
 	{
-		for (int p = 0; p <= 3; p++)
-		{
-			matrix.ml[p] = entities[i].matrix.m[0][p];
-		}
-		for (int p = 0; p <= 3; p++)
-		{
-			matrix.ml[p + 4] = entities[i].matrix.m[1][p];
-		}
-		for (int p = 0; p <= 3; p++)
-		{
-			matrix.ml[p + 8] = entities[i].matrix.m[2][p];
-		}
-		for (int p = 0; p <= 3; p++)
-		{
-			matrix.ml[p + 12] = entities[i].matrix.m[3][p];
-		}
-		glMultMatrixf(matrix.ml);
-		Sprite[0].Draw();
+		entities[i].draw();
 	}
-	glPopMatrix();
-	glClear(GL_COLOR_BUFFER_BIT);
 	SDL_GL_SwapWindow(displayWindow);
 }
 
